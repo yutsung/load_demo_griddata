a00ecc3dad3140044609a39dcf6d3da8c8d159a4
tsung <yutsung@manysplendid.com>
2024-12-09 10:37:53 +0800

    update units and ws10

--- data_loader.py
+++ data_loader.py
@@ -6,8 +6,8 @@ def load_0p01d_data(data_path='.'):
     u10 = np.fromfile(f'{data_path}/u10_binary_f4.bin', 'f4').reshape(581, 701)
     v10 = np.fromfile(f'{data_path}/v10_binary_f4.bin', 'f4').reshape(581, 701)
     tp  = np.fromfile(f'{data_path}/tp_binary_f4.bin',  'f4').reshape(581, 701)
-    return t2m, u10, v10, tp
+    return tp*1000, t2m-273.15, u10, v10, np.sqrt(u10**2 + v10**2)
 
 
 if __name__ == '__main__':
-    t2m, u10, v10, tp = load_0p01d_data()
+    tp, t2m, u10, v10, ws10 = load_0p01d_data()
